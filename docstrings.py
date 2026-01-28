2f682f9dcc72f33e160259955e5f66937ac6106a
Abasiono Alexander <ikangenyinabasiono@gmail.com>
2026-01-28 17:00:54 +0100

    Create docstrings.py

---
+++ docstrings.py
@@ -0,0 +1,35 @@
+# Add a docstring to count_letter()
+def count_letter(content, letter):
+  """ Count the number of times `letter` appears in `content`"""
+  if (not isinstance(letter, str)) or len(letter) != 1:
+    raise ValueError('`letter` must be a single character string.')
+  return len([char for char in content if char == letter])
+
+
+
+
+
+
+
+
+
+import inspect
+
+def build_tooltip(function):
+  """Create a tooltip for any function that shows the
+  function's docstring.
+
+  Args:
+    function (callable): The function we want a tooltip for.
+
+  Returns:
+    str
+  """
+  # Get the docstring for the "function" argument by using inspect
+  docstring = inspect.getdoc(function)
+  border = '#' * 28
+  return '{}\n{}\n{}'.format(border, docstring, border)
+
+print(build_tooltip(count_letter))
+print(build_tooltip(range))
+print(build_tooltip(print))
